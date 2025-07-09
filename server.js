8e00b34feba279d9f6bb18457ffe38aa6b91fa42
tonypw123 <134129781+tonypw123@users.noreply.github.com>
2025-07-09 01:08:10 -0500

    App Delivery

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
